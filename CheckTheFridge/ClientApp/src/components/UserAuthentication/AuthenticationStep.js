e5af937252174178ef2f5244bc57ae1a82801a6a
Adam Stagner <stagner2@pdx.edu>
2023-02-07 16:23:51 -0800

    generic login, working on create user/api

---
+++ CheckTheFridge/ClientApp/src/components/UserAuthentication/AuthenticationStep.js
@@ -0,0 +1,21 @@
+﻿import React,
+{ useState } from 'react';
+import App from '../../App';
+import LoginPage from './LoginPage';
+
+/*
+ * This file is the intermediate step for having a user login before viewing application
+ * currently a null token state is being passed to LoginPage component to be initialized to
+ * allow user to view webpage contents */
+
+export default function AuthorizeUser() {
+
+    const [token, setToken] = useState();
+
+    if (!token) {
+        return <LoginPage userToken={setToken} />    
+    }
+    return (
+       <App/>
+    )
+}
---
+++ CheckTheFridge/ClientApp/src/components/UserAuthentication/LoginPage.js
@@ -0,0 +1,39 @@
+﻿import React,
+{ useState } from 'react';
+import './LoginPage.css';
+
+export default function LoginPage({ userToken }) {
+
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    function handleSubmit () {
+        console.log(username);
+        console.log(password);
+        userToken(1);
+    }
+
+    return (
+      <form class = "login-form"  >
+          <h2> Check The Fridge </h2>
+          <div class = "input">
+              <label>
+                  <p>Username</p>
+                  <input type="text" onChange={e => setUsername(e.target.value)} />
+              </label>
+          </div>
+          <div class = "input">
+              <label>
+                  <p>Password</p>
+                  <input type="password" onChange={e => setPassword(e.target.value)} />
+              </label>
+
+          </div>
+          <div class = "submit">
+              <button type="submit" onClick={handleSubmit}>Submit</button>
+          </div>
+       </form>
+
+        )
+
+}
