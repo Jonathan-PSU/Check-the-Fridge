c89b8c673d603926b901b3d797750dcb2c12551a
Naya <44099724+nayamairena@users.noreply.github.com>
2023-02-26 22:43:32 -0800

    Clean up file code format

--- CheckTheFridge/ClientApp/src/components/UserAuthentication/LoginPage.js
+++ CheckTheFridge/ClientApp/src/components/UserAuthentication/LoginPage.js
@@ -1,181 +1,130 @@
 ﻿import React, { useState } from 'react';
 import {
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Container,
-  Row,
-  Col,
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Container,
+    Row,
+    Col,
 } from 'reactstrap';
 import './LoginPage.css';
 
 export default function LoginPage({ userToken }) {
-  //Variables
-  const [userID, setUserID] = useState(0);
-  const [first, setFirst] = useState();
-  const [last, setLast] = useState();
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
-  const [toggle, setToggle] = useState(false);
+    //Variables
+    const [userID, setUserID] = useState(0);
+    const [first, setFirst] = useState();
+    const [last, setLast] = useState();
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [toggle, setToggle] = useState(false);
 
-  // Function to check if username and password match in database. Returns response to token
-  async function login(uname, pass) {
-    if (uname === ' ' && pass === ' ')
-      //Guest Login for now
-      userToken(1);
-    else {
-      await fetch('ApplicationUser/Login/' + uname + '/' + pass, {
-        method: 'POST',
-      })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          }
-          userToken(0);
-          throw new Error('Something went wrong');
-        })
-        .then((responseJson) => {
-          setUserID(responseJson);
-          userToken(responseJson);
-          return responseJson;
-        })
-        .catch((error) => {
-          userToken(0);
-          console.log(error);
-        });
-    }
-  }
-  // Function to add a user to the database
-  async function createUser(fname, lname, uname, pass) {
-    await fetch(
-      'ApplicationUser/Register/' +
-        fname +
-        '/' +
-        lname +
-        '/' +
-        uname +
-        '/' +
-        pass,
-      { method: 'POST' }
-    )
-      .then((response) => {
-        if (response.ok) {
-          setToggle(false);
+    // Function to check if username and password match in database. Returns response to token
+    async function login(uname, pass) {
+        if (uname === ' ' && pass === ' ')
+            //Guest Login for now
+            userToken(1);
+        else {
+            await fetch('ApplicationUser/Login/' + uname + '/' + pass, { method: 'POST', })
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                    userToken(0);
+                    throw new Error('Something went wrong');
+                })
+                .then((responseJson) => {
+                    setUserID(responseJson);
+                    userToken(responseJson);
+                    return responseJson;
+                })
+                .catch((error) => {
+                    userToken(0);
+                    console.log(error);
+                });
         }
-        throw new Error('Not created');
-      })
+    }
+    // Function to add a user to the database
+    async function createUser(fname, lname, uname, pass) {
+        await fetch(
+            'ApplicationUser/Register/' + fname + '/' + lname + '/' + uname + '/' + pass, { method: 'POST' })
+            .then((response) => {
+                if (response.ok) {
+                    setToggle(false);
+                }
+                throw new Error('Not created');
+            })
 
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
-  // Function to send username and password to be verified.Expects user token.
-  function handleLogin(event) {
-    login(username, password);
-    event.preventDefault();
-    // Logic to be added for giving access or setting invalid message
-  }
+    // Function to send username and password to be verified.Expects user token.
+    function handleLogin(event) {
+        login(username, password);
+        event.preventDefault();
+        // Logic to be added for giving access or setting invalid message
+    }
 
-  /*Function that sends new user info to Backend to be added to database
-  Re-routes to login page */
-  function handleCreate(event) {
-    createUser(first, last, username, password);
-    event.preventDefault();
-  }
+    /*Function that sends new user info to Backend to be added to database
+    Re-routes to login page */
+    function handleCreate(event) {
+        createUser(first, last, username, password);
+        event.preventDefault();
+    }
 
-  /* Shows default login page/ sign up page */
-  return (
-    <>
-      <h2 style={{ textAlign: 'center' }}> Check The Fridge </h2>
-      <div className='border rounded m-5'>
-        {toggle ? (
-          <Form className='create-form'>
-            <FormGroup>
-              <Label for="fname">First Name:</Label>
-              <Input id="fname" type='text' autoComplete='off' onChange={(e) => setFirst(e.target.value)} />
-            </FormGroup>
-            <FormGroup>
-              <Label for="lname">Last Name:</Label>
-                          <Input id="lname" type='text' autoComplete='off' onChange={(e) => setLast(e.target.value)} />
-            </FormGroup>
-            <FormGroup>
-              <Label for="username">Username:</Label>
-              <Input id="username"
-                              type='text'
-                              autoComplete='off'
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="password">Password:</Label>
-              <Input id="password"
-                              type='password'
-                              autoComplete='off'
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </FormGroup>
-            <Row className='d-flex justify-content-center'>
-              <Col>
-                <Button
-                  style={{ width: '100%' }}
-                                  onClick={() => setToggle(false)}
-                  color='link'
-                >
-                  Already have an account?
-                </Button>
-              </Col>
-              <Col>
-                <Button
-                  style={{ width: '100%' }}
-                  type='submit'
-                  onClick={(event) => handleCreate(event)}
-                >
-                  Create
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        ) : (
-          <Form className='login-form'>
-            <FormGroup>
-              <Label for="username">Username:</Label>
-              <Input id="username"
-                type='text'
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="password">Password:</Label>
-              <Input id="password"
-                type='password'
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </FormGroup>
-            <Row className='d-flex justify-content-center'>
-              <Col>
-                <Button
-                  style={{ width: '100%' }}
-                  onClick={() => setToggle(true)}
-                >
-                  Create Account
-                </Button>
-              </Col>
-              <Col>
-                <Button
-                  style={{ width: '100%' }}
-                  type='submit'
-                  onClick={(event) => handleLogin(event)}
-                >
-                  Login
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        )}
-      </div>
-    </>
-  );
+    /* Shows default login page/ sign up page */
+    return (
+        <>
+            <h2 style={{ textAlign: 'center' }}> Check The Fridge </h2>
+            <div className='border rounded m-5'>
+                {toggle ? (<form className='create-form'>
+                    <FormGroup>
+                        <Label for="fname">First Name:</Label>
+                        <Input id="fname" name='fname' type='text' onChange={(e) => setFirst(e.target.value)} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="lname">Last Name:</Label>
+                        <Input id="lname" name='lname' type='text' onChange={(e) => setLast(e.target.value)} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="username">Username:</Label>
+                        <Input id="username" name='username' type='text' autoComplete='off' onChange={(e) => setUsername(e.target.value)} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password">Password:</Label>
+                        <Input id="password" name='password' type='password' autoComplete='off' onChange={(e) => setPassword(e.target.value)} />
+                    </FormGroup>
+                    <Row className='d-flex justify-content-center'>
+                        <Col>
+                            <Button style={{ width: '100%' }} onClick={() => setToggle(false)} color='link'>Already have an account?</Button>
+                        </Col>
+                        <Col>
+                            <Button style={{ width: '100%' }} type='submit' onClick={(event) => handleCreate(event)}>Create</Button>
+                        </Col>
+                    </Row>
+                </form>) :
+                    (<Form className='login-form'>
+                        <FormGroup>
+                            <Label for="username">Username:</Label>
+                            <Input id="username" name='username' type='text' onChange={(e) => setUsername(e.target.value)} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="password">Password:</Label>
+                            <Input id="password" name='password' type='password' onChange={(e) => setPassword(e.target.value)} />
+                        </FormGroup>
+                        <Row className='d-flex justify-content-center'>
+                            <Col>
+                                <Button style={{ width: '100%' }} onClick={() => setToggle(true)}>Create Account</Button>
+                            </Col>
+                            <Col>
+                                <Button style={{ width: '100%' }} type='submit' onClick={(event) => handleLogin(event)}>Login</Button>
+                            </Col>
+                        </Row>
+                    </Form>)}
+            </div>
+        </>
+    );
 }
