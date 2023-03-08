738abf3706f6826463991836bf29e4f57ff42f6b
Chase Verbout <cverbout@pdx.edu>
2023-03-08 10:25:42 -0800

    Change Ingredient + AppUser Model

--- CheckTheFridge/Models/Ingredient.cs
+++ CheckTheFridge/Models/Ingredient.cs
@@ -6,8 +6,10 @@ namespace CheckTheFridge.Models
     public class Ingredient
     {
         public int Id { get; set; }
+        public int MealDbId { get; set; } = 0;
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public int Quantity { get; set; } = 0;
+        public int ApplicationUserId { get; set; } = 0;
     }
 }
