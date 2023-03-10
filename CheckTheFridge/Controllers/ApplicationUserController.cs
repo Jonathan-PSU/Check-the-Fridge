9496189956a7c161879bde1fb52aada7c80a0f57
Chase Verbout <cverbout@pdx.edu>
2023-03-10 13:25:58 -0800

    Model change and Create Hash and Salt

--- CheckTheFridge/Controllers/ApplicationUserController.cs
+++ CheckTheFridge/Controllers/ApplicationUserController.cs
@@ -116,6 +116,27 @@ namespace CheckTheFridge.Controllers
         //PUTS
         //
 
+        //
+        //PRIVATE
+        //
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
         /* FORMERLY USING TEXT DATABASE
          * 
          * 
